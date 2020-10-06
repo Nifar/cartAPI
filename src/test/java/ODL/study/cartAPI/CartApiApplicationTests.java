6b455f1d5f38949366dcc06586e6c6ee1c26da40
Admin <C:\Users\Admin\AppData\Roaming\The Bat!>
2020-10-06 13:54:45 +0300

    Initial commit

---
+++ src/test/java/ODL/study/cartAPI/CartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ODL.study.cartAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
